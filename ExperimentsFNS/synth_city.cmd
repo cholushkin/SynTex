5ffab00fc5c52f52ccce7052789bd6099c7f7c8d
max <cholushkin@gmail.com>
2022-03-07 01:20:16 +0400

    + some more FNS experiments added.

---
+++ ExperimentsFNS/synth_city.cmd
@@ -0,0 +1,11 @@
+"../syntex.exe" verbose ExperimentsFNSOutput/db.csv FNS Samples/city.png ExperimentsFNSOutput/city1.png 3 128 32 4242
+"../syntex.exe" verbose ExperimentsFNSOutput/db.csv FNS Samples/city.png ExperimentsFNSOutput/city2.png 4 128 32 4242
+"../syntex.exe" verbose ExperimentsFNSOutput/db.csv FNS Samples/city.png ExperimentsFNSOutput/city3.png 5 128 32 4242
+
+"../syntex.exe" verbose ExperimentsFNSOutput/db.csv FNS Samples/city2.png ExperimentsFNSOutput/cityb1.png 3 128 32 4242
+"../syntex.exe" verbose ExperimentsFNSOutput/db.csv FNS Samples/city2.png ExperimentsFNSOutput/cityb2.png 4 128 32 4242
+"../syntex.exe" verbose ExperimentsFNSOutput/db.csv FNS Samples/city2.png ExperimentsFNSOutput/cityb3.png 5 128 32 4242
+
+"../syntex.exe" verbose ExperimentsFNSOutput/db.csv FNS Samples/city3.png ExperimentsFNSOutput/cityс1.png 3 128 32 4242
+"../syntex.exe" verbose ExperimentsFNSOutput/db.csv FNS Samples/city3.png ExperimentsFNSOutput/cityс2.png 4 128 32 4242
+"../syntex.exe" verbose ExperimentsFNSOutput/db.csv FNS Samples/city3.png ExperimentsFNSOutput/cityс3.png 5 128 32 4242
